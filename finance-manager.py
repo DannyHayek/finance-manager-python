27e30bd95ec3287728f39c01020789e758520de3
Danny <dhayek92@gmail.com>
2025-02-19 11:29:00 +0200

    Created basic framework for user input for salary, month and allocation

--- finance-manager.py
+++ finance-manager.py
@@ -4,3 +4,10 @@ allocation = [33, 33, 33]
 months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 print("Hello Nabiha!")
+
+currentSalary = int(input("Please input your salary for the month: "))
+currentMonth = int(input("Please input the current month (1-12): "))
+for i in range(3):
+    allocation[i] = int(input("Please input your salary allocations (Savings/Rent/Electricity): "))
+    
+
