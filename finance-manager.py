c1ceab0e1b7f3738db349ef2b349cb3d2a53fc0f
Danny <dhayek92@gmail.com>
2025-02-19 14:01:05 +0200

    Added dollar signs where necessary in output

--- finance-manager.py
+++ finance-manager.py
@@ -9,7 +9,7 @@ print("Hello Nabiha! Welcome to your finance manager.")
 
 while stop:
 
-    currentSalary = int(input("\nPlease input your salary for the month: "))
+    currentSalary = int(input("\nPlease input your salary for the month: $"))
     if currentSalary < 0 :
         print("Your salary cannot be negative! (Unless things are REALLY bad but at that point you need a lot more than a simple finance manager.)")
         continue
@@ -46,16 +46,17 @@ yearlyEstimate = yearlyRent + yearlyElec
 
 salaryPower = currentSalary ** 2
 
-randomSaving = 0
+extraSaving = 0
+extraDivided = 0
 
 print(f"\nMangaging finances for {months[currentMonth]}.")
-print(f"Salary for current month: {currentSalary}, allocated {allocation[0]}/{allocation[1]}/{allocation[2]} for savings, rent and electricity.")
+print(f"Salary for current month: ${currentSalary}, allocated {allocation[0]}/{allocation[1]}/{allocation[2]} for savings, rent and electricity.")
 print("\n1: Spendings on savings, rent and electricity")
 print("2: Your combined total spendings")
 print("3: What will remain after your spendings")
 print(f"4: Yearly estimates for rent and electricity based on {months[currentMonth]}")
 print("5: Your salary to the power of 2 (YOU WISH)")
-print("6: ")
+print("6: If you'd like to add a specific amount to savings")
 print("7: Quit the interface.")
 
 
@@ -63,22 +64,24 @@ option = int(input("\nPlease enter an option: "))
 
 while option != 7 :
     if option == 1 :
-        print(f"\nIn {months[currentMonth]}, you have allocated {moneyAllocation[0]} to savings, {moneyAllocation[1]} to rent and {moneyAllocation[2]} to electricity!" )
+        print(f"\nIn {months[currentMonth]}, you have allocated ${moneyAllocation[0]} to savings, ${moneyAllocation[1]} to rent and ${moneyAllocation[2]} to electricity!" )
 
     if option == 2 :
-        print(f"\nYour combined total spendings for {months[currentMonth]} amount to {monthTotal}.")
+        print(f"\nYour combined total spendings for ${months[currentMonth]} amount to {monthTotal}.")
 
     if option == 3 :
-        print(f"\nYou have {remainderSalary} left in {months[currentMonth]}.")
+        print(f"\nYou have ${remainderSalary} left in {months[currentMonth]}.")
 
     if option == 4 :
-        print(f"\nYour yearly estimate for rent is {yearlyRent}.")
-        print(f"Your yearly estimate for electricity is {yearlyElec}.")
-        print(f"Your combined yearly estimate for rent and electricity is {yearlyEstimate}.")
+        print(f"\nYour yearly estimate for rent is ${yearlyRent}.")
+        print(f"Your yearly estimate for electricity is ${yearlyElec}.")
+        print(f"Your combined yearly estimate for rent and electricity is ${yearlyEstimate}.")
 
 
     if option == 5 :
-        print(f"\nYour salary to the power of 2 would be {salaryPower}.")
+        print(f"\nYour salary to the power of 2 would be ${salaryPower}.")
+
+
 
 
     option = int(input("\nPlease enter another option: "))
