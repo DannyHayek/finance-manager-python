80bb86aa927d744a6b925581857ef7bf828bf679
Danny <dhayek92@gmail.com>
2025-02-19 11:25:26 +0200

    Initialized variables to be used

---
+++ finance-manager.py
@@ -0,0 +1,6 @@
+currentSalary = 0
+currentMonth = 0
+allocation = [33, 33, 33]
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+print("Hello Nabiha!")
