6b42d72cc979f28c68eaf3bd5ed38ee52ca3171f
Danny <dhayek92@gmail.com>
2025-02-19 11:39:20 +0200

    Added user input error handling to input framework

--- finance-manager.py
+++ finance-manager.py
@@ -2,12 +2,27 @@ currentSalary = 0
 currentMonth = 0
 allocation = [33, 33, 33]
 months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+stop = True             #boolean for error handling user input
 
 print("Hello Nabiha!")
 
-currentSalary = int(input("Please input your salary for the month: "))
-currentMonth = int(input("Please input the current month (1-12): "))
-for i in range(3):
-    allocation[i] = int(input("Please input your salary allocations (Savings/Rent/Electricity): "))
-    
+while stop:
+    currentSalary = int(input("Please input your salary for the month: "))
 
+    tempMonth = int(input("Please input the current month (1-12): "))
+    if 0 < tempMonth < 13:
+        currentMonth = tempMonth
+    else :
+        print("That is not a valid month! Restarting!")
+        continue
+    
+    for i in range(3):
+        allocation[i] = int(input("Please input your salary allocations (Savings/Rent/Electricity): "))
+
+    sum = allocation[0] +  allocation[1] + allocation[2]
+    if 98 < sum < 102 :             #Small error interval for human error
+        stop = False
+    else :
+        print("You still have unnallocated funds! Restarting!")
+    
+print(allocation)
