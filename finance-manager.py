4e934e0768a404c5ccd1c9b2ca95e3fb6b930625
Danny <dhayek92@gmail.com>
2025-02-20 12:32:04 +0200

    Fixed an issue where data for months was being replicated and saved multiple times

--- finance-manager.py
+++ finance-manager.py
@@ -1,15 +1,13 @@
 currentSalary = 0
 currentMonth = 0
-allocation = [33, 33, 33]
+allocation = []
 allocOptions = ["savings", "rent", "electricity"]
 months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 stopSingle = True             #boolean for error handling user input
 stopMonths = ""
 monthsList = []
-monthStorage = {
-    "month" : 0,
-    "salary" : 0
-}
+# monthStorage = {}
+
 
 print("Hello Nabiha! Welcome to your finance manager.")
 stopMonths = input("\nWould you like to start adding data to the manager? (Y/n) ")
@@ -19,105 +17,117 @@ while stopMonths != "n":
     stopSingle = True
 
     while stopSingle:
+        allocation = []
+
         tempMonth = int(input("\nPlease input the current month (1-12): "))
         if 0 < tempMonth < 13:
             currentMonth = tempMonth - 1    #For indexing
         else :
             print("That is not a valid month! Restarting!")
             continue
-        monthStorage["month"] = currentMonth
+        
 
 
         currentSalary = int(input("\nPlease input your salary for the month: $"))
         if currentSalary < 0 :
             print("Your salary cannot be negative! (Unless things are REALLY bad but at that point you need a lot more than a simple finance manager.)")
             continue
-        monthStorage["salary"] = currentSalary
         
         
         for i in range(3):
-            allocation[i] = int(input(f"\nPlease input your percentage salary allocation for {allocOptions[i]}: %"))
+            allocation.append(int(input(f"\nPlease input your percentage salary allocation for {allocOptions[i]}: %")))
 
         sum = allocation[0] +  allocation[1] + allocation[2]
         if sum < 102 :             #Small error interval for human error
             stopSingle = False
-            monthStorage["allocations"] = allocation
-            monthsList.append(monthStorage)
 
         else :
             print("You have overallocated funds! Restarting!")
+
+        monthStorage = {
+            "month" : currentMonth,
+            "salary" : currentSalary,
+            "allocations" : allocation
+        }
+
+        monthsList.append(monthStorage)
     
     stopMonths = input("\nWould you like to add any more data? (Y/n) ")
     
-# print(monthsList)
+print(monthsList)
 
-workingMonth = int(input("\nWhich month would you like to work on (1-12): ")) - 1
+# workingMonth = int(input("\nWhich month would you like to work on (1-12): ")) - 1
 
-print(workingMonth)
+# print(workingMonth)
 
-if 0 < workingMonth < 13 :
-        
+# for i in range(len(monthsList)) :
+#     if monthsList[i]["month"] == workingMonth :
+#         print(months[i])
 
-    moneyAllocation = [0, 0, 0]
-    for i in range(3) :
-        moneyAllocation[i] = allocation[i] * currentSalary / 100
+# if 0 < workingMonth < 13 :
+#     for i in range(len(monthsList)) :
+#         if monthsList[]
 
-    monthTotal =  moneyAllocation[0] + moneyAllocation[1] + moneyAllocation[2]
+#         moneyAllocation = [0, 0, 0]
+#         for i in range(3) :
+#             moneyAllocation[i] = allocation[i] * currentSalary / 100
 
-    remainderSalary = currentSalary - monthTotal
+#         monthTotal =  moneyAllocation[0] + moneyAllocation[1] + moneyAllocation[2]
 
-    yearlyRent = moneyAllocation[1] * 12
-    yearlyElec = moneyAllocation[2] * 12
+#         remainderSalary = currentSalary - monthTotal
 
-    yearlyEstimate = yearlyRent + yearlyElec
+#         yearlyRent = moneyAllocation[1] * 12
+#         yearlyElec = moneyAllocation[2] * 12
 
-    salaryPower = currentSalary ** 2
+#         yearlyEstimate = yearlyRent + yearlyElec
 
-    extraSaving = 0
-    extraDivided = 0
+#         salaryPower = currentSalary ** 2
 
-    print(f"\nMangaging finances for {months[currentMonth]}.")
-    print(f"Salary for current month: ${currentSalary}, allocated {allocation[0]}/{allocation[1]}/{allocation[2]} for savings, rent and electricity.")
-    print("\n1: Spendings on savings, rent and electricity")
-    print("2: Your combined total spendings")
-    print("3: What will remain after your spendings")
-    print(f"4: Yearly estimates for rent and electricity based on {months[currentMonth]}")
-    print("5: Your salary to the power of 2")
-    print("6: If you'd like to add a specific amount to savings")
-    print("7: Quit the interface.")
+#         extraSaving = 0
+#         extraDivided = 0
 
-    option = int(input("\nPlease enter an option: "))
+#         print(f"\nMangaging finances for {months[currentMonth]}.")
+#         print(f"Salary for current month: ${currentSalary}, allocated {allocation[0]}/{allocation[1]}/{allocation[2]} for savings, rent and electricity.")
+#         print("\n1: Spendings on savings, rent and electricity")
+#         print("2: Your combined total spendings")
+#         print("3: What will remain after your spendings")
+#         print(f"4: Yearly estimates for rent and electricity based on {months[currentMonth]}")
+#         print("5: Your salary to the power of 2")
+#         print("6: If you'd like to add a specific amount to savings")
+#         print("7: Quit the interface.")
 
-    while option != 7 :
-        if option == 1 :
-            print(f"\nIn {months[currentMonth]}, you have allocated ${moneyAllocation[0]} to savings, ${moneyAllocation[1]} to rent and ${moneyAllocation[2]} to electricity!" )
+#         option = int(input("\nPlease enter an option: "))
 
-        elif option == 2 :
-            print(f"\nYour combined total spendings for {months[currentMonth]} amount to ${monthTotal}.")
+#         while option != 7 :
+#             if option == 1 :
+#                 print(f"\nIn {months[currentMonth]}, you have allocated ${moneyAllocation[0]} to savings, ${moneyAllocation[1]} to rent and ${moneyAllocation[2]} to electricity!" )
 
-        elif option == 3 :
-            print(f"\nYou have ${remainderSalary} left in {months[currentMonth]}.")
+#             elif option == 2 :
+#                 print(f"\nYour combined total spendings for {months[currentMonth]} amount to ${monthTotal}.")
 
-        elif option == 4 :
-            print(f"\nYour yearly estimate for rent is ${yearlyRent}.")
-            print(f"Your yearly estimate for electricity is ${yearlyElec}.")
-            print(f"Your combined yearly estimate for rent and electricity is ${yearlyEstimate}.")
+#             elif option == 3 :
+#                 print(f"\nYou have ${remainderSalary} left in {months[currentMonth]}.")
+
+#             elif option == 4 :
+#                 print(f"\nYour yearly estimate for rent is ${yearlyRent}.")
+#                 print(f"Your yearly estimate for electricity is ${yearlyElec}.")
+#                 print(f"Your combined yearly estimate for rent and electricity is ${yearlyEstimate}.")
 
 
-        elif option == 5 :
-            print(f"\nYour salary to the power of 2 would be ${salaryPower}.")
+#             elif option == 5 :
+#                 print(f"\nYour salary to the power of 2 would be ${salaryPower}.")
 
-        elif option == 6 :
-            extraSaving = int(input("\nPlease enter the extra savings: $"))
-            if moneyAllocation[0] != 0 :
-                extraDivided = extraSaving / moneyAllocation[0]
-            print(f"The result of this operation is: ${extraDivided}")
+#             elif option == 6 :
+#                 extraSaving = int(input("\nPlease enter the extra savings: $"))
+#                 if moneyAllocation[0] != 0 :
+#                     extraDivided = extraSaving / moneyAllocation[0]
+#                 print(f"The result of this operation is: ${extraDivided}")
 
-        else :
-            print("\nPlease enter a valid option!")
+#             else :
+#                 print("\nPlease enter a valid option!")
 
-        option = int(input("\nPlease enter another option: "))
+#             option = int(input("\nPlease enter another option: "))
 
-print("\nSee you next month!\n")
+# print("\nSee you next month!\n")
 
         
