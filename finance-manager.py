e71f3c10fe9712c91921a94a6ebbfefa3df26af4
Danny <dhayek92@gmail.com>
2025-02-20 12:51:14 +0200

    Completed framework for which allows the user to select which month to perform operations on

--- finance-manager.py
+++ finance-manager.py
@@ -6,7 +6,7 @@ months = ["January", "February", "March", "April", "May", "June", "July", "Augus
 stopSingle = True             #boolean for error handling user input
 stopMonths = ""
 monthsList = []
-# monthStorage = {}
+monthStorage = {}
 
 
 print("Hello Nabiha! Welcome to your finance manager.")
@@ -56,78 +56,87 @@ while stopMonths != "n":
     
 print(monthsList)
 
-# workingMonth = int(input("\nWhich month would you like to work on (1-12): ")) - 1
+operations = input("\nWould you like to perform any operations on a month's salary? (Y/n) ")
 
 # print(workingMonth)
 
 # for i in range(len(monthsList)) :
 #     if monthsList[i]["month"] == workingMonth :
-#         print(months[i])
+#         print(months[workingMonth])
 
-# if 0 < workingMonth < 13 :
-#     for i in range(len(monthsList)) :
-#         if monthsList[]
+while operations != "n" :
+    workingMonth = int(input("\nWhich month would you like to work on (1-12): ")) - 1
 
-#         moneyAllocation = [0, 0, 0]
-#         for i in range(3) :
-#             moneyAllocation[i] = allocation[i] * currentSalary / 100
-
-#         monthTotal =  moneyAllocation[0] + moneyAllocation[1] + moneyAllocation[2]
-
-#         remainderSalary = currentSalary - monthTotal
-
-#         yearlyRent = moneyAllocation[1] * 12
-#         yearlyElec = moneyAllocation[2] * 12
-
-#         yearlyEstimate = yearlyRent + yearlyElec
-
-#         salaryPower = currentSalary ** 2
-
-#         extraSaving = 0
-#         extraDivided = 0
-
-#         print(f"\nMangaging finances for {months[currentMonth]}.")
-#         print(f"Salary for current month: ${currentSalary}, allocated {allocation[0]}/{allocation[1]}/{allocation[2]} for savings, rent and electricity.")
-#         print("\n1: Spendings on savings, rent and electricity")
-#         print("2: Your combined total spendings")
-#         print("3: What will remain after your spendings")
-#         print(f"4: Yearly estimates for rent and electricity based on {months[currentMonth]}")
-#         print("5: Your salary to the power of 2")
-#         print("6: If you'd like to add a specific amount to savings")
-#         print("7: Quit the interface.")
-
-#         option = int(input("\nPlease enter an option: "))
-
-#         while option != 7 :
-#             if option == 1 :
-#                 print(f"\nIn {months[currentMonth]}, you have allocated ${moneyAllocation[0]} to savings, ${moneyAllocation[1]} to rent and ${moneyAllocation[2]} to electricity!" )
-
-#             elif option == 2 :
-#                 print(f"\nYour combined total spendings for {months[currentMonth]} amount to ${monthTotal}.")
-
-#             elif option == 3 :
-#                 print(f"\nYou have ${remainderSalary} left in {months[currentMonth]}.")
-
-#             elif option == 4 :
-#                 print(f"\nYour yearly estimate for rent is ${yearlyRent}.")
-#                 print(f"Your yearly estimate for electricity is ${yearlyElec}.")
-#                 print(f"Your combined yearly estimate for rent and electricity is ${yearlyEstimate}.")
+    if 0 < workingMonth < 13 :
+        for i in range(len(monthsList)) :
+            if monthsList[i]["month"] == workingMonth :
+                currentSalary = monthsList[i]["salary"]
+                allocation = monthsList[i]["allocations"]
 
 
-#             elif option == 5 :
-#                 print(f"\nYour salary to the power of 2 would be ${salaryPower}.")
+        moneyAllocation = [0, 0, 0]
+        for i in range(3) :
+            moneyAllocation[i] = allocation[i] * currentSalary / 100
 
-#             elif option == 6 :
-#                 extraSaving = int(input("\nPlease enter the extra savings: $"))
-#                 if moneyAllocation[0] != 0 :
-#                     extraDivided = extraSaving / moneyAllocation[0]
-#                 print(f"The result of this operation is: ${extraDivided}")
+        monthTotal =  moneyAllocation[0] + moneyAllocation[1] + moneyAllocation[2]
 
-#             else :
-#                 print("\nPlease enter a valid option!")
+        remainderSalary = currentSalary - monthTotal
 
-#             option = int(input("\nPlease enter another option: "))
+        yearlyRent = moneyAllocation[1] * 12
+        yearlyElec = moneyAllocation[2] * 12
 
-# print("\nSee you next month!\n")
+        yearlyEstimate = yearlyRent + yearlyElec
+
+        salaryPower = currentSalary ** 2
+
+        extraSaving = 0
+        extraDivided = 0
+
+        print(f"\nMangaging finances for {months[workingMonth]}.")
+        print(f"Salary for current month: ${currentSalary}, allocated {allocation[0]}/{allocation[1]}/{allocation[2]} for savings, rent and electricity.")
+        print("\n1: Spendings on savings, rent and electricity")
+        print("2: Your combined total spendings")
+        print("3: What will remain after your spendings")
+        print(f"4: Yearly estimates for rent and electricity based on {months[workingMonth]}")
+        print("5: Your salary to the power of 2")
+        print("6: If you'd like to add a specific amount to savings")
+        print("7: Quit the interface.")
+
+        option = int(input("\nPlease enter an option: "))
+
+        while option != 7 :
+            if option == 1 :
+                print(f"\nIn {months[workingMonth]}, you have allocated ${moneyAllocation[0]} to savings, ${moneyAllocation[1]} to rent and ${moneyAllocation[2]} to electricity!" )
+
+            elif option == 2 :
+                print(f"\nYour combined total spendings for {months[workingMonth]} amount to ${monthTotal}.")
+
+            elif option == 3 :
+                print(f"\nYou have ${remainderSalary} left in {months[workingMonth]}.")
+
+            elif option == 4 :
+                print(f"\nYour yearly estimate for rent is ${yearlyRent}.")
+                print(f"Your yearly estimate for electricity is ${yearlyElec}.")
+                print(f"Your combined yearly estimate for rent and electricity is ${yearlyEstimate}.")
+
+
+            elif option == 5 :
+                print(f"\nYour salary to the power of 2 would be ${salaryPower}.")
+
+            elif option == 6 :
+                extraSaving = int(input("\nPlease enter the extra savings: $"))
+                if moneyAllocation[0] != 0 :
+                    extraDivided = extraSaving / moneyAllocation[0]
+                print(f"The result of this operation is: ${extraDivided}")
+
+            else :
+                print("\nPlease enter a valid option!")
+
+            option = int(input("\nPlease enter another option: "))
+    
+    operations = input("\nWould you like to perform any operations on another month's salary? (Y/n) ")
+
+
+print("\nSee you next month!\n")
 
         
